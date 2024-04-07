6499bc3948b7480a9b7705c0b973d04376b8d322
jwang1006 <147784250+jwang1006@users.noreply.github.com>
2024-04-07 14:58:05 -0700

    ball pong working

--- pythonCourse/day_22.py
+++ pythonCourse/day_22.py
@@ -1,61 +1,110 @@
-import turtle, random
+import turtle, random, time
 
 class Paddle:
     def __init__(self, x):
-        self.pieces = []
-        self.addPiece(x, -20)
-        self.addPiece(x, 0)
-        self.addPiece(x, 20)
+        self.x: int = x
+        self.score: int = 0
+        self.direction: str = "none"
+        self.paddle = self.createPiece()
 
-    def addPiece(self, x, y):
+    def createPiece(self):
         toAdd = turtle.Turtle()
+        toAdd.hideturtle()
         toAdd.penup()
         toAdd.color("white")
         toAdd.shape("square")
+        toAdd.shapesize(stretch_wid=1, stretch_len=5)
         toAdd.speed(0)
         toAdd.left(90)
-        toAdd.setposition(x, y)
-        self.pieces.append(toAdd)
+        toAdd.setposition(self.x, -50)
+        toAdd.showturtle()
+        return toAdd
     
-    def moveUp(self):
-        for piece in self.pieces:
-            piece.forward(10)
+    def getScore(self) -> int:
+        return self.score
     
-    def moveDown(self):
-        for piece in self.pieces:
-            piece.backward(10)
+    def scorePoint(self):
+        self.score+=1
+
+    def directionUp(self):
+        self.direction = "up"
+
+    def directionDown(self):
+        self.direction = "down"
+
+    def directionNone(self):
+        self.direction = "none"
+    
+    def movePaddle(self):
+        if self.direction=="up":
+            self.paddle.sety(self.paddle.ycor()+20)
+        elif self.direction=="down":
+            self.paddle.sety(self.paddle.ycor()-20)
+    
+    def hitPaddle(self, y):
+        return self.paddle.ycor()+50>y and self.paddle.ycor()-50<y
 
 ball = turtle.Turtle()
 ball.penup()
 ball.color("white")
 ball.shape("circle")
-
-score1 = 0
-score2 = 0
+ball.speed(0)
 screen = turtle.Screen()
 screen.bgcolor("black")
-screen.setup(600,  600)
-paddle1 = Paddle(-280)
-paddle2 = Paddle(280)
+screen.setup(800,  600)
+paddle1 = Paddle(-390)
+paddle2 = Paddle(380)
 
-screen.delay(10)
 screen.listen()
-screen.onkeypress(paddle1.moveUp, "w")
-screen.onkeypress(paddle1.moveDown, "s")
-screen.onkeypress(paddle2.moveUp, "o")
-screen.onkeypress(paddle2.moveDown, "k")
+screen.onkeypress(paddle1.directionUp, "w")
+screen.onkeypress(paddle1.directionDown, "s")
+screen.onkeypress(paddle2.directionUp, "Up")
+screen.onkeypress(paddle2.directionDown, "Down")
+screen.onkeyrelease(paddle1.directionNone, "w")
+screen.onkeyrelease(paddle1.directionNone, "s")
+screen.onkeyrelease(paddle2.directionNone, "Up")
+screen.onkeyrelease(paddle2.directionNone, "Down")
+turtle.penup()
+turtle.hideturtle()
+turtle.sety(260)
+turtle.color("white")
+
+def startRound():
+    turtle.clear()
+    turtle.write(f"{paddle1.getScore(), paddle2.getScore()}", align="center", font=("Arial", 25, "normal"))
+    ball.hideturtle()
+    ball.home()
+    ball.showturtle()
+    direction = random.randint(0, 3)
+    if direction==0:
+        ball.right(random.randint(0, 45))
+    elif direction==1:
+        ball.left(random.randint(0, 45))
+    elif direction==2:
+        ball.right(random.randint(135, 180))
+    elif direction==3:
+        ball.left(random.randint(135, 180))
+
 
 def calculateDirection():
-    angle = ball.heading()
-    print(angle)
     if ball.ycor()>290:
-        angle = (ball.heading()-90)*2       
-    elif ball.ycor()<-290:
-        angle = (ball.heading()-270)*2
-    elif ball.xcor()<-290:
         angle = (ball.heading()-180)*2
-    elif ball.xcor()>290:
+    elif ball.ycor()<-290:
         angle = ball.heading()*2
+    elif ball.xcor()<-380:
+        if paddle1.hitPaddle(ball.ycor()):
+            angle = (ball.heading()-90)*2
+        else:
+            paddle2.scorePoint()
+            startRound()
+            return
+    elif ball.xcor()>380:
+        if paddle2.hitPaddle(ball.ycor()):
+            angle = (ball.heading()-270)*2
+        else:
+            paddle1.scorePoint()
+            startRound()
+            return
     else:
         return
     if angle>0:
@@ -63,11 +112,15 @@ def calculateDirection():
     else:
         angle*=-1
         ball.left(angle)
-    print(angle)
 
-ball.right(random.randint(0, 360))
-while True:
-    ball.forward(10)
-    calculateDirection()
+def gameloop():
+    startRound()
+    while True:
+        ball.forward(10)
+        paddle1.movePaddle()
+        paddle2.movePaddle()
+        calculateDirection()
+        screen.update()
+        time.sleep(0.02)
 
-screen.exitonclick()
+gameloop()
