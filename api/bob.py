8ee5e5b4b86983faf7c80b165c9856b961e8717d
jwang1006 <147784250+jwang1006@users.noreply.github.com>
2024-06-10 20:53:51 +0900

    pls god

---
+++ api/bob.py
@@ -0,0 +1,3 @@
+conan = "sk-j8SpdtyoQYp0JSCJ6QGRT3BlbkFJ6AfzC2oPH7K5jEBbZM2v1"
+gray = "AIzaSyDygpJd18_Q2Hivu8YWxh31TotCQ1u4wmg1"
+
--- api/gpt_test.py
+++ api/gpt_test.py
@@ -1,17 +1,16 @@
-import openai
+import openai, bob
 
-client = openai.OpenAI(api_key="key here")
+client = openai.OpenAI(api_key=bob.conan)
 
-# Non-streaming:
-print("----- standard request -----")
-completion = client.chat.completions.create(
+stream = client.chat.completions.create(
     model="gpt-4-turbo",
     messages=[
-        {
-            "role": "user",
-            "content": "Say this is a test",
-        },
+        {"role": "user", "content": "Say this is a test"},
     ],
+    stream=True,  # Add this line
 )
-print(completion.choices[0].message.content)
 
+for chunk in stream:
+    chunk_message = chunk.choices[0] # Get the Choice object
+    if chunk_message.finish_reason is None: # Check if the response is still being generated
+        print(chunk_message.delta.content or "", end="") # Handle potential None values
